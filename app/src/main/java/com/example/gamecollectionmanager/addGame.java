b96697190e7c02223ac8319c34a7f03019eaee3d
MrQbert <65645542+MrQbert@users.noreply.github.com>
2024-12-04 12:44:20 -0600

    new
    started adding info

--- app/src/main/java/com/example/gamecollectionmanager/addGame.java
+++ app/src/main/java/com/example/gamecollectionmanager/addGame.java
@@ -1,5 +1,6 @@
 package com.example.gamecollectionmanager;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,9 +15,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class addGame extends AppCompatActivity {
     EditText enterNameEl;
     EditText enterPlatformEl;
-    Button submit;
     SQLiteDatabase db;
-    StringBuilder sb;
+    StringBuilder sb = new StringBuilder();
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +27,27 @@ public class addGame extends AppCompatActivity {
         enterNameEl = findViewById(R.id.enterNameEl);
         enterPlatformEl = findViewById(R.id.enterPlatformEl);
         db = openOrCreateDatabase("gameCollection", MODE_PRIVATE, null);
-        db.execSQL("create table if not exists games ()");
+        db.execSQL("create table if not exists games (gameName varchar(50),gameConsole varchar(50));");
 
 
     }
+    public void save(View v){
+        String gameName = enterNameEl.getText().toString().trim();
+        String gameConsole = enterPlatformEl.getText().toString().trim();
+
+        if(gameName.length() == 0 || gameConsole.length() ==0){
+            sb.delete(0,sb.length());
+            sb.append("Please Enter All the Fields");
+            ShowMessage("Error", sb.toString());
+        }else{
+            db.execSQL("insert into games(gameName, gameConsole) Values('" + gameName + "','" + gameConsole + "' )");
+            sb.delete(0,sb.length());
+            sb.append("Happy Gaming");
+            ShowMessage("Added!", sb.toString());
+        }
+
+    }
+
     public void toHome(View v){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
@@ -37,5 +55,13 @@ public class addGame extends AppCompatActivity {
     public void toSearch(View v){
         Intent i = new Intent(this, searchGame.class);
         startActivity(i);
+    }
+
+    public void ShowMessage(String title, String message){
+        android.app.AlertDialog.Builder messBr = new android.app.AlertDialog.Builder(this);
+        messBr.setCancelable(true);
+        messBr.setTitle(title);
+        messBr.setMessage(message);
+        messBr.show();
     }
 }
